18fe0b24903df85861dd1e06db9f0b063434dc96
Pournima Gadekar <113817343+Pournima310@users.noreply.github.com>
2023-12-15 14:17:46 +0530

    MaxNumberfinder.java

--- MaxNumberfinder.java
+++ MaxNumberfinder.java
@@ -1,22 +1,26 @@
-import java.util.list;
-public class MaxNumberfinder{
-    public static int findMax(List<Object>array ){
-        int max=Integer.MIN_VALUE;
-        for(Object element:array){
-            if(element instance of list){ 
-                int maxInSubArray=findMax(List<Object>)element);
-                max=Math.max(max,maxInSubArray); 
-            } 
-            else if (element instance Integer)
-            {
-                max=Math.Max(max,(Integer)element);
+import java.util.List;
+
+public class MaxNumberFinder {
+
+    public static int findMax(List<Object> array) {
+        int max = Integer.MIN_VALUE;
+
+        for (Object element : array) {
+            if (element instanceof List) {
+                int maxInSubArray = findMax((List<Object>) element);
+                max = Math.max(max, maxInSubArray);
+            } else if (element instanceof Integer) {
+                max = Math.max(max, (Integer) element);
             }
-            }
-            return max;
         }
-        public static void main(string[]args){
-            List<Object>inputArray=List.Of(2,4,10,List.Of(12,4,List.Of(100,99),4List.Of(3,2,99),0);
-            int result=findMax(inputArray);
-            system.out.println("Maximum Number:"+result)
-;
-                }        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        List<Object> inputArray = List.of(2, 4, 10, List.of(12, 4, List.of(100, 99), 4), List.of(3, 2, 99), 0);
+
+        int result = findMax(inputArray);
+        System.out.println("Maximum Number: " + result);
+    }
+}
