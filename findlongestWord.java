67be97887f87525fab7f97fb3053c0b5814cace1
Pournima Gadekar <113817343+Pournima310@users.noreply.github.com>
2023-12-15 14:23:13 +0530

    findlongestWord.java

--- findlongestWord.java
+++ findlongestWord.java
@@ -1,31 +1,33 @@
-public class LongestWordfinder {
-    public static void main(sting[]args){
-        string input1="dummy text of the printing and typesetting industry.";
-        string output1=findlongestWord(input1);
-        system.out.println("Input:\""+ input+"\"");
-        system.out.println("output:"+output1);
-        
-        String input2="It is a long-established fact that a reader will be distracted by the readable content of"+
-        "a page when looking at its layout. The point of using Lorem Ipsum is that it has a"+
-        "more-or-less normal distributionqqqqqqqqqqqqqqqqqqq of letters, as opposed to using "+
-        "'Content here', making it look like readable English”;
-        string output2 =findLongestWord(input2);
-        system.out.println("\nInput:\""+input2+"\"");
-        System.out.println("output:"+ output2);
-    } 
-    private static string findlongestWord(String input){
-        string[]words =input.split("\\s+");
-        string longestWord=""; 
+public class LongestWordFinder {
 
-        for (string word:words){ 
-            string cleanword =word.replaceAll("[^a-zA-Z]","");
+    public static void main(String[] args) {
+        String input1 = "dummy text of the printing and typesetting industry.";
+        String output1 = findLongestWord(input1);
+        System.out.println("Input: \"" + input1 + "\"");
+        System.out.println("Output: " + output1);
 
-            if(cleanword.length()>LongestWord.length()) { 
+        String input2 = "It is a long-established fact that a reader will be distracted by the readable content of " +
+                "a page when looking at its layout. The point of using Lorem Ipsum is that it has a " +
+                "more-or-less normal distributionqqqqqqqqqqqqqqqqqqq of letters, as opposed to using " +
+                "'Content here, content here', making it look like readable English";
+        String output2 = findLongestWord(input2);
+        System.out.println("\nInput: \"" + input2 + "\"");
+        System.out.println("Output: " + output2);
+    }
+
+    private static String findLongestWord(String input) {
+        String[] words = input.split("\\s+");
+        String longestWord = "";
+
+        for (String word : words) {
+            // Remove non-alphabetic characters for better accuracy
+            String cleanWord = word.replaceAll("[^a-zA-Z]", "");
+
+            if (cleanWord.length() > longestWord.length()) {
+                longestWord = cleanWord;
             }
         }
+
         return longestWord;
     }
 }
-
-
-
